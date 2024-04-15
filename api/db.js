8840d0c69cddba1a0ef367f6e52d6e276540f828
Mondo3771 <kabelomrn@gmail.com>
2024-04-15 11:31:32 +0200

    added envoromental variables

--- api/db.js
+++ api/db.js
@@ -1,11 +1,11 @@
 const sql = require("mssql");
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
+  user: "azureuser",
+  password: "30012004Kb.",
+  server: "mysqlserverkabza.database.windows.net",
   port: 1433,
-  database: process.env.DB_NAME,
+  database: "SD_Project",
   authentication: {
     type: "default",
   },
