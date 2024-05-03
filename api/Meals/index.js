6b4ca66eabe72543c0ec52ac6fc5140afe731572
Mondo3771 <kabelomrn@gmail.com>
2024-05-03 16:51:19 +0200

    Added API

--- api/Meals/index.js
+++ api/Meals/index.js
@@ -6,27 +6,33 @@ module.exports = async function (context, req) {
   const data = req.body;
   switch (req.method) {
     case "GET":
-      if(req.body.Meal_ID === undefined|| req.body.Meal_ID===null ){try {
-        const resultSet = await pool.request().query(`SELECT * FROM Meals`);
-        context.res = {
-          status: 200,
-          body: {
-            data: resultSet.recordset,
-            message: "Successfully retrieved meals",
-          },
-        };
-      } catch (err) {
-        context.res = {
-          status: 500,
-          message: "Error connecting to the database",
-          body: {},
-        };
-        console.error("Error running query", err);
-      }}
-      else{
+      console.log(req.body.Meal_ID);
+      if (req.body.Emp_ID_ID === undefined || req.body.Emp_ID === null) {
         try {
-          const resultSet = await pool.request().input("Emp_ID", sql.Int, data.Emp_ID)
-          .query(`SELECT * Meals.* From Bookings JOIN Meals ON Bookings.Meal_ID = Meals.Meal_ID Where Booking.Emp_ID = Emp_ID`);
+          const resultSet = await pool.request().query(`SELECT * FROM Meals`);
+          context.res = {
+            status: 200,
+            body: {
+              data: resultSet.recordset,
+              message: "Successfully retrieved meals",
+            },
+          };
+        } catch (err) {
+          context.res = {
+            status: 500,
+            message: "Error connecting to the database",
+            body: {},
+          };
+          console.error("Error running query", err);
+        }
+      } else {
+        try {
+          const resultSet = await pool
+            .request()
+            .input("Emp_ID", sql.Int, data.Emp_ID)
+            .query(
+              `SELECT Meals.* From Bookings JOIN Meals ON Bookings.Meal_ID = Meals.Meal_ID Where Bookings.Emp_ID = @Emp_ID`
+            );
           context.res = {
             status: 200,
             body: {
@@ -43,53 +49,13 @@ module.exports = async function (context, req) {
           console.error("Error running query", err);
         }
       }
-      
-      break;
-
-    case "PUT":
-      // console.log(data);
-      try {
-        if (
-          data.Meal_ID === undefined ||
-          data.Meal_ID === "" ||
-          data.Meal_ID === null
-        ) {
-          context.res = {
-            status: 400,
-            message: "ID cannot be null or empty",
-            body: {},
-          };
-        } else {
-          // If the ID exists, update the Emp_type
-          const resultSet = await pool
-            .request()
-            .input("Emp_ID", sql.Int, data.Emp_ID)
-            .input("Emp_type", sql.NVarChar, data.EMP_type)
-            .query(
-              `UPDATE Employees SET EMP_type = @Emp_type WHERE Emp_ID = @Emp_ID`
-            );
-
-          context.res = {
-            status: 200,
-            message: "Employee type updated successfully",
-            body: {},
-          };
-        }
-      } catch (err) {
-        context.res = {
-          status: 500,
-          message: "Error inserting data into the database",
-          body: {},
-        };
-        console.error("Error running query", err);
-      }
       break;
     case "DELETE":
       try {
         if (
-          data.Emp_ID === undefined ||
-          data.Emp_ID === "" ||
-          data.Emp_ID === null
+          data.Booking_ID === undefined ||
+          data.Booking_ID === "" ||
+          data.Booking_ID === null
         ) {
           context.res = {
             status: 400,
@@ -97,15 +63,10 @@ module.exports = async function (context, req) {
             body: {},
           };
         } else {
-          // If the ID exists, delete the row
-          //these are 2 seperates queries that i do in one line
           const resultSet = await pool
             .request()
-            .input("Emp_ID", sql.Int, data.Emp_ID)
-            .query(
-              `DELETE FROM Tasks WHERE Emp_ID = @Emp_ID ;
-                DELETE FROM Employees WHERE Emp_ID = @Emp_ID`
-            );
+            .input("Bookings_ID", sql.Int, data.Booking_ID)
+            .query(`DELETE FROM Bookings WHERE Booking_ID = @Bookings_ID `);
           context.res = {
             staus: 200,
             body: { message: "Deleted" },
@@ -125,7 +86,7 @@ module.exports = async function (context, req) {
         if (
           data.Meal_ID === undefined ||
           data.Meal_ID === "" ||
-          data.Meal_ID === null||
+          data.Meal_ID === null ||
           data.Emp_ID === undefined ||
           data.Emp_ID === "" ||
           data.Emp_ID === null
@@ -140,14 +101,16 @@ module.exports = async function (context, req) {
           const resultSet = await pool
             .request()
             .input("Emp_ID", sql.Int, data.Emp_ID)
-            .input("Emp_type", sql.NVarChar, data.EMP_type)
+            .input("Meal_ID", sql.Int, data.Meal_ID)
+            .input("Date_of_booking", sql.Date, data.Date_of_booking)
             .query(
-              `UPDATE Employees SET EMP_type = @Emp_type WHERE Emp_ID = @Emp_ID`
+              `INSERT INTO Bookings (Emp_ID, Meal_ID, Date_of_booking) 
+             VALUES (@Emp_ID, @Meal_ID, @Date_of_booking)`
             );
 
           context.res = {
             status: 200,
-            message: "Employee type updated successfully",
+            message: "Booking type Deleted successfully",
             body: {},
           };
         }
