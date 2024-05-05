0203131040ab5c33c29579f15b8fb8fde68058d4
Mondo3771 <kabelomrn@gmail.com>
2024-05-05 18:20:11 +0200

    added date

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,77 +3,67 @@ import "./Modal.css";
 
 // import { PostBooking } from "../Carousel/fetch";
 
-function Modal({ setOpenModal, data ,employee, booking}) {
+function Modal({ setOpenModal, data, employee, booking }) {
   const confirmBooking = () => {
-
-   const PostBooking = (Emp_ID, Meal_ID) => {
+    const PostBooking = (Emp_ID, Meal_ID) => {
       fetch(`/api/CreateMeals`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ "Emp_ID": Emp_ID, "Meal_ID": Meal_ID, "Date": new Date().toISOString()}),
+        body: JSON.stringify({
+          Emp_ID: Emp_ID,
+          Meal_ID: Meal_ID,
+          Date_of_Booking: new Date().toISOString(),
+        }),
       })
         .then((response) => response.json())
         .then((data) => {
-
           console.log(data);
-          
+
           // Do something with your data
         });
     };
-    PostBooking(employee.Emp_ID,data.Meal_ID);
+    PostBooking(employee.Emp_ID, data.Meal_ID);
 
-
-
-      setOpenModal(false);
+    setOpenModal(false);
   };
 
   return (
     <main className="modalBackground">
       <section className="modalContainer">
-        
-        {booking?
-        <>
-        <article className="already">
-        <p className="already">You already have a booking</p>
-        <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            Close
-          </button>
-
-        </article>
-          
-        
-
-        </>
-
-        
-      
-      :
-      <>
-         <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
-          </button>
-        </div>
-       <h2>{data.Name_of_Meal}</h2>
-        <p>Description: {data.Description}</p>
-        {/* <p>Available: {data.Available ? "Yes" : "No"}</p> */}
-        <section className="section">
-          <button onClick={confirmBooking}>Confirm Booking</button>
-        </section>
-      </>
-     
-      }
-        
+        {booking ? (
+          <>
+            <article className="already">
+              <p className="already">You already have a booking</p>
+              <button
+                onClick={() => {
+                  setOpenModal(false);
+                }}
+              >
+                Close
+              </button>
+            </article>
+          </>
+        ) : (
+          <>
+            <div className="titleCloseBtn">
+              <button
+                onClick={() => {
+                  setOpenModal(false);
+                }}
+              >
+                X
+              </button>
+            </div>
+            <h2>{data.Name_of_Meal}</h2>
+            <p>Description: {data.Description}</p>
+            {/* <p>Available: {data.Available ? "Yes" : "No"}</p> */}
+            <section className="section">
+              <button onClick={confirmBooking}>Confirm Booking</button>
+            </section>
+          </>
+        )}
       </section>
     </main>
   );
