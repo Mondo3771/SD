8e9611aaa14ee58326e33b4423ff0a55e7888738
Nathan Joseph <nathanjoseph069@gmail.com>
2024-05-05 21:07:46 +0200

    removed meals

--- src/pages/Meals/Meals.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import background from "../../Images/Bckgrd.svg";
-
-export const Wrapper = styled.section`
-
-background-image: url(${background});
-width: 100%;
-height:100%;
-`;
