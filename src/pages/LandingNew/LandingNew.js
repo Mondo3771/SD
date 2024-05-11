b1011847aa76ad49611eb701ba9cbccaeb1af11e
Mondo3771 <kabelomrn@gmail.com>
2024-05-11 15:13:22 +0200

    Did something

--- src/pages/LandingNew/LandingNew.js
+++ src/pages/LandingNew/LandingNew.js
@@ -22,7 +22,6 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Index from "../../routes/Index";
 
 import Loader from "../../components/Loader/Loader";
-import { response } from "express";
 
 const LandingNew = () => {
   const history = useHistory();
@@ -40,26 +39,28 @@ const LandingNew = () => {
     setLoaded(true);
   };
   console.log(user);
+  // Need to find a way to get the Management API token
   const token =
-    "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxlQmsyZXlFWUR0MGNram9QT1EzUSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xeWNyMmY0YnJlYTRtcW4wLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtY0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtMXljcjJmNGJyZWE0bXFuMC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTcxNTQyMjM4NSwiZXhwIjoxNzE1NTA4Nzg1LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJhenAiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtYyJ9.SE0CVCoHOYyVYc4GEGMoQfzo6EhsDf04iDbHG86ku16Fm3H8eyMNkiVQ7rnkE2eCkwo7pi9lb_iUPlfhUf1PjUXG-3lTSjbiGM4hMiOzdOBHlybwr_gcpLEdR2jXR-E6GyusiLn0TBMCsJO7DFkHL_llVCkht0zX8dEQI0ZWcxwEuAHFY0eyLgt9Rvhkww6o1XqA3RywI7AOB-o8fG7dXl1yds5xPA1yN_rKgh6JI-4NCgy5J478YycdD05aHX_1D0abrcak-PQ4bNP7y8AZmek_kZ-NJ7LMFnxLAx8NRwOlU3X5gHW_D-JBGMxxY10317lugXTviv_Ak3PFBCdzLw";
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxlQmsyZXlFWUR0MGNram9QT1EzUSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xeWNyMmY0YnJlYTRtcW4wLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtY0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtMXljcjJmNGJyZWE0bXFuMC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTcxNTQyMjM4NSwiZXhwIjoxNzE1NTA4Nzg1LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJhenAiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtYyJ9.SE0CVCoHOYyVYc4GEGMoQfzo6EhsDf04iDbHG86ku16Fm3H8eyMNkiVQ7rnkE2eCkwo7pi9lb_iUPlfhUf1PjUXG-3lTSjbiGM4hMiOzdOBHlybwr_gcpLEdR2jXR-E6GyusiLn0TBMCsJO7DFkHL_llVCkht0zX8dEQI0ZWcxwEuAHFY0eyLgt9Rvhkww6o1XqA3RywI7AOB-o8fG7dXl1yds5xPA1yN_rKgh6JI-4NCgy5J478YycdD05aHX_1D0abrcak-PQ4bNP7y8AZmek_kZ-NJ7LMFnxLAx8NRwOlU3X5gHW_D-JBGMxxY10317lugXTviv_Ak3PFBCdzLw";
   // we are working on gewtting the users Role from Auh0
-  const getRole = () => {
-    fetch(
-      `https://dev-1ycr2f4brea4mqn0.us.auth0.com/api/v2/users/${user.sub}/roles`,
-      {
-        header: {
-          Authorization: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxlQmsyZXlFWUR0MGNram9QT1EzUSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xeWNyMmY0YnJlYTRtcW4wLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtY0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtMXljcjJmNGJyZWE0bXFuMC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTcxNTQyMjM4NSwiZXhwIjoxNzE1NTA4Nzg1LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJhenAiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtYyJ9.SE0CVCoHOYyVYc4GEGMoQfzo6EhsDf04iDbHG86ku16Fm3H8eyMNkiVQ7rnkE2eCkwo7pi9lb_iUPlfhUf1PjUXG-3lTSjbiGM4hMiOzdOBHlybwr_gcpLEdR2jXR-E6GyusiLn0TBMCsJO7DFkHL_llVCkht0zX8dEQI0ZWcxwEuAHFY0eyLgt9Rvhkww6o1XqA3RywI7AOB-o8fG7dXl1yds5xPA1yN_rKgh6JI-4NCgy5J478YycdD05aHX_1D0abrcak-PQ4bNP7y8AZmek_kZ-NJ7LMFnxLAx8NRwOlU3X5gHW_D-JBGMxxY10317lugXTviv_Ak3PFBCdzLw`,
-        },
-      }
-    )
+  const getRole = (gtoken) => {
+    fetch(`https://dev-1ycr2f4brea4mqn0.us.auth0.com/api/v2/users`, {
+      headers: {
+        // Authorization: `Bearer ${gtoken}`,
+        Authorization:
+          "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkxlQmsyZXlFWUR0MGNram9QT1EzUSJ9.eyJpc3MiOiJodHRwczovL2Rldi0xeWNyMmY0YnJlYTRtcW4wLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtY0BjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtMXljcjJmNGJyZWE0bXFuMC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTcxNTQyMjM4NSwiZXhwIjoxNzE1NTA4Nzg1LCJzY29wZSI6InJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIHJlYWQ6cm9sZXMgY3JlYXRlOnJvbGVzIGRlbGV0ZTpyb2xlcyB1cGRhdGU6cm9sZXMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJhenAiOiJSZTlscVhDY05ZWTJSdUY0eGhiTnROZHlOM3dkWXNtYyJ9.SE0CVCoHOYyVYc4GEGMoQfzo6EhsDf04iDbHG86ku16Fm3H8eyMNkiVQ7rnkE2eCkwo7pi9lb_iUPlfhUf1PjUXG-3lTSjbiGM4hMiOzdOBHlybwr_gcpLEdR2jXR-E6GyusiLn0TBMCsJO7DFkHL_llVCkht0zX8dEQI0ZWcxwEuAHFY0eyLgt9Rvhkww6o1XqA3RywI7AOB-o8fG7dXl1yds5xPA1yN_rKgh6JI-4NCgy5J478YycdD05aHX_1D0abrcak-PQ4bNP7y8AZmek_kZ-NJ7LMFnxLAx8NRwOlU3X5gHW_D-JBGMxxY10317lugXTviv_Ak3PFBCdzLw",
+      },
+    })
       .then((response) => response.json())
       .then((Role) => {
         console.log(Role);
       });
   };
-  // if (isAuthenticated && !Loaded) {
-  //   childToParent(user);
-  // }
+  if (isAuthenticated && !Loaded) {
+    // childToParent(user);
+    console.log("user", user.sub);
+    getRole(token);
+  }
 
   const login = () => {
     fetch(`/api/login?Email=${data.email}&Token=${data.sub}`)
