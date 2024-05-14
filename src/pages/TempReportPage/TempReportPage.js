5ddccd42a8c392f33eb168b1eab7a6a66980bd60
Mondo3771 <kabelomrn@gmail.com>
2024-05-14 14:14:42 +0200

    Adde tne fectg

--- src/pages/TempReportPage/TempReportPage.js
+++ src/pages/TempReportPage/TempReportPage.js
@@ -17,15 +17,30 @@ export const TempReportPage = () => {
   const [Receiver, setReceiver] = useState({});
   const [AllFeedback, setAllFeedBack] = useState(MockFeedBack);
   const [firstLoad, setFirstLoad] = useState(false);
-
+  const Emp_ID = 83;
   useEffect(() => {
     // Get All the info you need on this page once (AllFeedback,AllUsers)
     // Get from storage
     setLocalStorage({ key: "Users", value: MockUsers });
-    setLocalStorage({ key: "Feedback", value: MockFeedBack });
-    setReceiver(MockUsers[2]);
+
+    const feedback = () => {
+      fetch(`/api/feedback?Emp_ID=${Emp_ID}`, {
+        method: "Get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setAllFeedBack(data);
+          setLocalStorage({ key: "Feedback", value: data });
+        });
+    };
+    feedback();
+
+    // setReceiver(MockUsers[2]);
     setUsers(MockUsers);
-    setAllFeedBack(MockFeedBack);
     setFirstLoad(true);
   }, []);
 
@@ -41,11 +56,10 @@ export const TempReportPage = () => {
   const handleSendFeedback = (sender, receiver, message) => {
     //create new message to be sent into the database
     // Database should give back the Message_ID
-    const Message_ID = Math.random()*1000;
+    const Message_ID = Math.random() * 1000;
     const Date = "2015-08-08";
-    console.log("Sender",sender);
-    console.log("Receiver",receiver);
-
+    console.log("Sender", sender);
+    console.log("Receiver", receiver);
 
     const feedback = {
       Message_ID: Message_ID,
@@ -55,11 +69,10 @@ export const TempReportPage = () => {
       Date: Date,
       Message: message,
     };
-    const storageChange = [...fetchStorageData({key: "Feedback"}),feedback];
-    setLocalStorage({key: "Feedback",value: storageChange})
-    setAllFeedBack(prev =>  [feedback,...prev]);
-    toast.success(`Message successfully sent to ${receiver.Name}!`)
-
+    const storageChange = [...fetchStorageData({ key: "Feedback" }), feedback];
+    setLocalStorage({ key: "Feedback", value: storageChange });
+    setAllFeedBack((prev) => [feedback, ...prev]);
+    toast.success(`Message successfully sent to ${receiver.Name}!`);
   };
 
   return (
